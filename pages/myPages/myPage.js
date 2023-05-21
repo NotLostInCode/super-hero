74e76614c59390e4d9519a85d4713d6a27910f2c
Gregory <not.lost.in.code@gmail.com>
2023-05-21 22:19:31 +0400

    styling search

--- pages/myPages/myPage.js
+++ pages/myPages/myPage.js
@@ -1,35 +1,101 @@
 export default function pageScript(page) {
-  const input = document.querySelector(".form__search");
-  const buttonEl = document.querySelector(".form__btn");
-  const form = document.querySelector(".form");
-  const container = document.querySelector(".bla");
+  // const input = document.querySelector(".form__search");
+  // const buttonEl = document.querySelector(".form__btn");
+  // const form = document.querySelector(".form");
+  // const container = document.querySelector(".bla");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  // form.addEventListener("submit", (e) => {
+  //   e.preventDefault();
 
-    const inputValue = input.value;
+  //   const inputValue = input.value;
 
-    const searchUrl = `https://www.superheroapi.com/api.php/553827756740754/search/${inputValue}`;
+  //   const searchUrl = `https://www.superheroapi.com/api.php/553827756740754/search/${inputValue}`;
 
-    return fetch(searchUrl)
-      .then((response) => response.json())
-      .then((data) => {
+  //   return fetch(searchUrl)
+  //     .then((response) => response.json())
+  //     .then((data) => {
 
 
-        function renderHeroes() {
-          const div = document.createElement("div");
-          const img = document.createElement("img");
-          const result = data.results[0].image.url;
-          img.src = result;
-          container.appendChild(div);
-          div.appendChild(img);
-        }
-        renderHeroes();
+  //       function renderHeroes() {
+  //         const div = document.createElement("div");
+  //         const img = document.createElement("img");
+  //         const result = data.results[0].image.url;
+  //         img.src = result;
+  //         container.appendChild(div);
+  //         div.appendChild(img);
+  //       }
+  //       renderHeroes();
 
-        // console.log(data.results[1])
-      })
-      .catch((error) =>
-        console.log("Ошибка при получение API в myPage.js", error)
-      );
-  });
+  //       // console.log(data.results[1])
+  //     })
+  //     .catch((error) =>
+  //       console.log("Ошибка при получение API в myPage.js", error)
+  //     );
+  // });
+
+
+
+   // Динамический поиск
+
+
+
+
+
+
+    // const formHero = document.querySelector('.form-display__hero')
+    // const searchInput = document.querySelector('.search')
+    // const searchListHero = document.querySelector('.list__hero')
+
+    // async function loadHeroes(searchHeroes) {
+    //   const URL = `https://www.superheroapi.com/api.php/553827756740754/search/${searchHeroes}`
+    //   const response = await fetch(URL)
+    //   const data = await response.json()
+    //   console.log(data)
+    // }
+
+    // function findHeroes() {
+    //   let searchHeroes = (searchInput.value).trim()
+    //   loadHeroes(searchHeroes)
+    // }
+
+    // function displayHeroesList(heroes) {
+
+    // }
+
+    //  formHero.addEventListener('submit', (e) => {
+    //   e.preventDefault()
+
+    //   const inputValue = searchInput.value
+    //   const searchUrl = `https://www.superheroapi.com/api.php/553827756740754/search/${inputValue}`;
+
+    //   fetch(searchUrl)
+    //    .then(response => response.json())
+    //    .then((data) => {
+    //     const heroResult = data.results
+    //     heroResult.forEach(superHero => {
+    //       hero.push(superHero)
+    //     })
+    //    })
+    //    console.log(hero)
+
+      //  function getListHero (word, stations) {
+      //   return stations.filter((s) => {
+
+      //     const regex = new RegExp(word, 'gi')
+      //     return s.
+      //   })
+      //  }
+    //  })
+
+
+
+    // function getListHero () {
+
+    // }
+
+    // function displayHero () {
+
+    // }
+
+
 }
