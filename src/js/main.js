c61e713aa1e31c814fbecafee20025e3b1a898db
Gregory <not.lost.in.code@gmail.com>
2023-11-25 21:18:17 +0400

    Implementation of search

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,7 @@
 import '../style/style.css'
+import md5 from 'js-md5'
 
 
-
-// import md5 from 'js-md5'
-
-// const publicKey = 'dcf493d56816d73d4ae6a5645dcbb219'
-// const privateKey = 'a01ebb5323b7bcfb24739f70ee22f56726f7f0e4'
-// const timestamp = new Date().getTime().toString()
-// const hash = md5(timestamp + privateKey + publicKey)
-// const apiUrl = `https://gateway.marvel.com/v1/public/characters?limit=20&apikey=${publicKey}&ts=${timestamp}&hash=${hash}`
-
-// fetch(apiUrl)
-// 	.then(response => {
-// 		if (!response.ok) {
-// 			throw new Error(`Could not fetch ${apiUrl}, status: ${response.status}`)
-// 		}
-// 		return response.json()
-// 	})
-// 	.then(data => console.log(data))
-// 	.catch(error => console.error(error))
 const spoilerHeaders = document.querySelectorAll('.spoiler__header')
 
 spoilerHeaders.forEach(spoilerHeader => {
@@ -29,18 +12,59 @@ spoilerHeaders.forEach(spoilerHeader => {
 })
 
 // search====================
-	const searchingResults = document.querySelector('.searching__results')
 
-	// create img hero
-	const heroImg = document.createElement('img')
-	heroImg.src = 'https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg'
-	heroImg.classList.add('searching-results__img')
 
-	// create name hero
-	const heroName = document.createElement('p')
-	heroName.textContent = 'Герой'
-	heroName.classList.add('searching-results__name')
+	const searchingResultsShell = document.querySelector('.searching-results__shell')
+	const searchInput = document.querySelector('.search__input')
 
-	//adding block search results
-	searchingResults.append(heroImg, heroName)
+
+
+
+
+	const publicKey = 'dcf493d56816d73d4ae6a5645dcbb219'
+	const privateKey = 'a01ebb5323b7bcfb24739f70ee22f56726f7f0e4'
+	const timestamp = new Date().getTime().toString()
+	const hash = md5(timestamp + privateKey + publicKey)
+	const apiUrl = `https://gateway.marvel.com/v1/public/characters?&apikey=${publicKey}&ts=${timestamp}&hash=${hash}`
+	// const apiUrl = `https://gateway.marvel.com/v1/public/characters?name=${searchInput.value}&apikey=${publicKey}&ts=${timestamp}&hash=${hash}`
+
+	fetch(apiUrl)
+		.then(response => response.json())
+		.then(data => {
+			console.log(data.data.results)
+
+
+
+
+			data.data.results.forEach(hero => {
+				// create shell search results
+				const searchingResults = document.createElement('div')
+				searchingResults.classList.add('searching__results')
+
+				// create img hero
+				const heroImg = document.createElement('img')
+				heroImg.classList.add('searching-results__img')
+				heroImg.src = `${hero.thumbnail.path}.${hero.thumbnail.extension}`
+
+				// create name hero
+				const heroName = document.createElement('p')
+				heroName.classList.add('searching-results__name')
+				heroName.textContent = hero.name
+
+				searchingResults.append(heroImg, heroName)
+
+				searchingResultsShell.append(searchingResults)
+			})
+
+
+		})
+
+
+
+		// const searchHero = (hero) => {
+		// 	searchInput.addEventListener('input', function () {
+		// 		console.log(searchInput.value)
+		// 	})
+		// }
+
 // /search====================
