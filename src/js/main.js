a938bde48100e4e695b867e89270757a6d10b72b
Gregory <not.lost.in.code@gmail.com>
2023-11-30 13:17:47 +0400

    refactoring the header

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,32 @@ import md5 from 'js-md5'
 
 const spoilerHeaders = document.querySelectorAll('.spoiler__header')
 
+
+
+
+//test header
+const element = document.getElementById("animate");
+
+if (element) {
+	// reset the transition by...
+	element.addEventListener("click", function(e) {
+		e.preventDefault;
+
+		console.log('element', element.classList);
+
+		// removing the class
+		element.classList.remove("run-animation");
+
+		// triggering reflow
+		void element.offsetWidth;
+
+		// and re-adding the class
+		element.classList.add("run-animation");
+	}, false);
+}
+
+//  test header
+
 spoilerHeaders.forEach((spoilerHeader) => {
 	spoilerHeader.addEventListener('click', () => {
 		const spoilerContent = spoilerHeader
@@ -11,6 +37,8 @@ spoilerHeaders.forEach((spoilerHeader) => {
 		spoilerContent.classList.toggle('visible')
 	})
 })
+
+
 
 // search====================
 
@@ -73,3 +101,5 @@ const findHeroes = () => {
 searchInput.addEventListener('input', findHeroes)
 
 // /search====================
+
+
